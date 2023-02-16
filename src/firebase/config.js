4350cae02d7b0a5d004a95ed2cb1e915e74c3ff9
sh1nn0on3 <sanogaku31@gmail.com>
2023-02-16 13:09:40 +0700

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,8 @@ import "firebase/compat/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAszvws9bqn5pmlaM9ZLVniOOAXKntq8xE",
